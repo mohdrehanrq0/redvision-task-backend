c4e48fc33d4651bdd2d92f73c4cc11eba94b5833
Mohammad Rehan Qureshi <53863785+mohdrehanrq0@users.noreply.github.com>
2024-08-22 22:59:16 +0530

    change cors error

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,3 @@
 export const corsWhitelist = [`http://localhost:3000`]; // Allow frontend testing url
-export const corsRegex = /\.redvisiontechnologies.com$/;
+export const corsRegex = /\.netlify.app$/;
 export const environment = process.env.NODE_ENV;
